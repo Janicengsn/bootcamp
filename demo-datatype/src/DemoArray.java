f0eef3d99e56f69bf0e01e46d467b711d9ecf202
janicengsn <janicensn0610@gmail.com>
2025-04-26 21:49:54 +0800

    update on 26.04.2025

--- demo-datatype/src/DemoArray.java
+++ demo-datatype/src/DemoArray.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class DemoArray {
   public static void main(String[] args) {
     // problem 
@@ -28,15 +30,114 @@ public class DemoArray {
     // declare double array -> 4 values
     // 1.1, 9.2, -9.8, 77.9
     // print them out
-    double[] arr = new int [4];
-    arr[0] = 1.1;
-    arr[1] = 9.2;
-    arr[2] = -9.8;
-    arr[3] = 77.9;
-    System.out.println(arr[0]);
-    System.out.println(arr[1]);
-    System.out.println(arr[2]);
-    System.out.println(arr[3]);
+    double[] arr2 = new double [4];
+    arr2[0] = 1.1;
+    arr2[1] = 9.2;
+    arr2[2] = -9.8;
+    arr2[3] = 77.9;
+    System.out.println(arr2[0]);
+    System.out.println(arr2[1]);
+    System.out.println(arr2[2]);
+    System.out.println(arr2[3]);
+
+    // arr2.length -> array length
+    // for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < arr2.length; i++) {
+      System.out.println(arr2[i]);
+    }
+
+
+    // String[]
+    String[] arr3 = new String[3];
+    arr3[0] = "abc";
+    arr3[1] = "def";
+    arr3[2] = "bootcamp";
+
+    String[] arr4 = new String[] {"abc","def","bootcamp"};
+    // for loop
+    for (int i = 0; i < arr4.length; i++) { // i < 3 -> 0,1,2
+      System.out.println(arr4[i]);
+    }
+    // for loop + if (filtering)
+    // Find the strings in the array, which length > 4
+    for (int i = 0; i < arr4.length; i++) {
+      if (arr4[i].length() > 4) {
+        System.out.println(arr4[i]);
+      }
+    }
+    // for loop + if (AND OR)
+    // Print the strings in the array, which length > 4 and starts with 'b'
+    for (int i = 0; i < arr4.length; i++) {
+      if(arr4[i].length() > 4 && arr4[i].startsWith("b")) {
+        System.out.println(arr4[i]);
+      }
+    }
+    // Print the strings in the array, which length = 3 or endsWith 'p'
+    for (int i = 0; i < arr4.length; i++) {
+      if(arr4[i].length() == 3 || arr4[i].endsWith("p")) {
+        System.out.println(arr4[i]);
+      }
+    }
+    // 
+    int[] arr5 = new int[] {4, 7, -3, 1500, -100, 1000};
+    // Print the max value in the array. -> 多左個value, 搵到個result會同下一關有影響
+    // for loop + if
+    int box = arr5[0];
+    for (int i = 0; i < arr5.length; i++) {
+      if (arr5[i] > box) {
+        box = arr5[i];
+      }
+    }
+    System.out.println(box);
+   // Print the min value in the array.
+    int min = arr5[0];
+    for (int i = 0; i < arr5.length; i++) {
+     if (arr5[i] < min) {
+      min = arr5[i];
+    }
+   }
+    System.out.println(min);
+
+
+    //swap
+    int a = 6;
+    int b = 8;
+    int backup = a;
+    a = b;
+    b = backup;
+    System.out.println(a); // 8
+    System.out.println(b); //6
+
+    int[] arr6 = new int[] {9, 7, 10, -3, 8};
+    // ! Move the max value to the tail of the array.
+    // for + if
+    for (int i = 0; i < arr6.length - 1; i++) {
+      if (arr6[i] > arr6[i + 1]) {
+        // swap
+        backup = arr6[i];
+        arr6[i] = arr6[i + 1];
+        arr6[i + 1] = backup;
+      }
+    }
+    System.out.println(Arrays.toString(arr6)); // [7, 9, -3, 8, 10]
+
+    // Sorting
+    // [7, 9, -3, 8, 10]
+    // [x, x, x, 9, 10]
+    // ...
+    // [-3, 7, 8, 9, 10]
+    arr6 = new int [] {9, 7, 10, -3, 8};
+    for (int i = 0; i < arr6.length - 1; i++) { // how many round
+      for (int j = 0; j < arr6.length - 1 - i; j++) { //
+        if (arr6[j] > arr6[j + 1]) {
+          backup = arr6[j];
+          arr6[j] = arr6[j + 1];
+          arr6[j + 1] = backup;
+        }
+      }
+    }
+    System.out.println(Arrays.toString(arr6)); // [-3, 7, 8, 9, 10]
+
+
   }
-  
 }
--- demo-datatype/src/DemoIf.java
+++ demo-datatype/src/DemoIf.java
@@ -1,31 +1,32 @@
 public class DemoIf {
-  public static void main(String[]args){
+  public static void main(String[] args) {
     int x = 15;
 
     if (x < 4) {
       System.out.println("hello");
     }
 
-    if(x < 10) {
+    if (x < 10) {
       System.out.println("goodbye");
-    }else{
+    } else {
       System.out.println("welcome");
     }
 
     // ! Rules:
-    //Step 1: try "if" condition, if it is true, execute the action, and then EXIT.
-    //Step 2: if the Step 1 result is false, then "else if" condition, if it is true, execute the action, and then EXIT.
-    //Step 3: if step result is false, then goes to "else", execute the action.
+    // Step 1: try "if" condition, if it is true, execute the action, and then EXIT.
+    // Step 2: if the Step 1 result is false, then "else if" condition, if it is
+    // true, execute the action, and then EXIT.
+    // Step 3: if step result is false, then goes to "else", execute the action.
 
     // if + else if + else
     int y = 100;
 
-    if(y < 90) {
+    if (y < 90) {
       System.out.println("y is smaller than 90.");
     } else if (y >= 90 && y < 100) {
       System.out.println("y is 90 < y 99");
     } else { // y >= 100}
-     System.out.println("y >= 100");
+      System.out.println("y >= 100");
     }
 
     // double w
@@ -36,9 +37,9 @@ public class DemoIf {
     int w = 40;
     if (w < 100) {
       System.out.println("ABC");
-    }else if (w >=103 && w <=110) {
+    } else if (w >= 103 && w <= 110) {
       System.out.println("DEF");
-    }else {
+    } else {
       System.out.println("XYZ");
     }
 
@@ -48,7 +49,7 @@ public class DemoIf {
     int b = 15;
     if (a > 3 || b < 15) {
       System.out.println("a > 3 OR b < 15.");
-    }else {
+    } else {
       System.out.println("Error"); // print
     }
 
@@ -56,20 +57,20 @@ public class DemoIf {
     int u = 80;
     if (q > 2) {
       u = u + 2; // execute
-     } else if (q < 5){
-       u = u + 10; // not execute
-     }
-     System.out.println(u); // 82
+    } else if (q < 5) {
+      u = u + 10; // not execute
+    }
+    System.out.println(u); // 82
 
-     q = 10;
-     u = 100;
-     if (q > 5 && u < 99) {
+    q = 10;
+    u = 100;
+    if (q > 5 && u < 99) {
       System.out.println("hello");
-     } else if (q <= 5) {
-       System.out.println("bye");
-     } else { // q > 5 && u >= 99
-       System.out.println("bootcamp");
-  }
+    } else if (q <= 5) {
+      System.out.println("bye");
+    } else { // q > 5 && u >= 99
+      System.out.println("bootcamp");
+    }
 
     // String
     String s1 = "hel";
@@ -85,11 +86,11 @@ public class DemoIf {
     String s2 = "Javascript";
     int score = 0;
     if (s2.equals("Java")) {
-      score +=1;
+      score += 1;
     } else {
-      score +=2;
+      score += 2;
     }
-    System.out.println(score); //2
+    System.out.println(score); // 2
 
     // charAt
     // Check if the first character of string is 'c' or the last character is 'd',
@@ -100,13 +101,14 @@ public class DemoIf {
       System.out.println("yes");
     } else {
       System.out.println("no");
+      // You cannot use switch to handle the above scenario.
+    }
 
-
-      String s4 = "hello world";
-      if (s4.charAt(0) == 'c' || s4.charAt(s4.length() - 1) == 'd') {
-        System.out.println("yes");
-        } else {
-        System.out.println("no");
+    String s4 = "hello world";
+    if (s4.charAt(0) == 'c' || s4.charAt(s4.length() - 1) == 'd') {
+      System.out.println("yes");
+    } else {
+      System.out.println("no");
     }
 
     // switch
@@ -115,7 +117,7 @@ public class DemoIf {
       System.out.println("Grade is A");
     } else if (grade == 'B') {
       System.out.println("Grade is B"); // print
-    } else if (grade == 'C'){
+    } else if (grade == 'C') {
       System.out.println("Grade is C");
     } else {
       System.out.println("Fail.");
@@ -125,18 +127,18 @@ public class DemoIf {
     // ! 2. switch CANNOT check the range of the value
     switch (grade) {
       case 'A':
-      System.out.println("Grade is A");
-      break;
+        System.out.println("Grade is A");
+        break;
       case 'B':
-      System.out.println("Grade is B"); // print
-      break;
+        System.out.println("Grade is B"); // print
+        break;
       case 'C':
-      System.out.println("Grade is C");
-      break;
+        System.out.println("Grade is C");
+        break;
       default:
-      System.out.println("Fail");
+        System.out.println("Fail");
     }
-
+    System.out.println("helllo");
     score = 85;
     if (score >= 90) {
       grade = 'A';
@@ -148,11 +150,8 @@ public class DemoIf {
       grade = 'F';
     }
 
-    // You cannot use switch to handle the above scenario.
-   }
+
+    
+    
   }
 }
-
-
-  
-
